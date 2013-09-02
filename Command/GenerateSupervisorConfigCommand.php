fce94dc515b796807d898a5ac5d5f4a47d7621b3
gedi <gediminas.morkevicius@gmail.com>
2013-09-02 11:37:32 +0300

    do not use http server for supervisord config allow to specify number of workers to run and superwise

--- Command/GenerateSupervisorConfigCommand.php
+++ Command/GenerateSupervisorConfigCommand.php
@@ -3,6 +3,7 @@
 namespace Supertag\Bundle\GearmanBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,10 +12,7 @@ class GenerateSupervisorConfigCommand extends ContainerAwareCommand
 {
     const NAME = 'supertag:gearman:generate-supervisor-config';
 
-    const TEMPLATE = <<<EOF
-[unix_http_server]
-file=%project_dir%/supervisord.sock
-
+    const TEMPLATE_MAIN = <<<EOF
 [supervisord]
 logfile=%root_dir%/logs/supervisord.log
 logfile_maxbytes=50MB
@@ -25,26 +23,21 @@ nodaemon=false
 minfds=1024
 minprocs=200
 
-; the below section must remain in the config file for RPC
-; (supervisorctl/web interface) to work, additional interfaces may be
-; added by defining them in separate rpcinterface: sections
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+%workers%
+EOF;
 
-[supervisorctl]
-serverurl=unix:///%project_dir%/supervisord.sock ; use a unix:// URL  for a unix socket
-
-[program:worker]
+    const TEMPLATE_WORKER = <<<EOF
+[program:worker%num_worker%]
 command=/usr/bin/php %project_dir%/app/console supertag:gearman:run-worker --env=%env%
-process_name=%namespace%gearman_worker
+process_name=%namespace%gearman_worker%num_worker%
 numprocs=1
 directory=%project_dir%
 autostart=true
 autorestart=true
 user=%user%
-stdout_logfile=%root_dir%/logs/gearman-worker.info.log
+stdout_logfile=%root_dir%/logs/gearman-worker%num_worker%.info.log
 stdout_logfile_maxbytes=1MB
-stderr_logfile=%root_dir%/logs/gearman-worker.error.log
+stderr_logfile=%root_dir%/logs/gearman-worker%num_worker%.error.log
 stderr_logfile_maxbytes=1MB
 EOF;
 
@@ -54,12 +47,13 @@ EOF;
             ->setName(self::NAME)
             ->setDescription('Generate a supervisor config, based on current symfony2 project')
             ->addArgument('user', InputArgument::REQUIRED)
+            ->addOption('num-workers', null, InputOption::VALUE_REQUIRED, "Specifies a number of parallel workers")
             ->setHelp(<<<EOF
 The <info>%command.name%</info> command generates supervisor config to watch
 over gearman workers, respawn them in case of unexpected errors:
 
 <info>php %command.full_name% www-data</info>
-<info>php %command.full_name% --env=prod www-data</info>
+<info>php %command.full_name% --env=prod www-data --num-worrkers=2</info>
 EOF
         );
     }
@@ -72,11 +66,26 @@ EOF
         $env = $this->getContainer()->getParameter('kernel.environment');
         $namespace = $this->getContainer()->getParameter('supertag_gearman.namespace');
 
+        $numWorkers = $input->hasOption('num-workers') ? intval($input->getOption('num-workers')) : 1;
+        if ($numWorkers <= 0) {
+            $numWorkers = 1;
+        }
+        $workers = '';
         $replace = compact('project_dir', 'root_dir', 'user', 'env', 'namespace');
+        for ($i = 0; $i < $numWorkers; $i++) {
+            $replace['num_worker'] = $i + 1;
+            $workers .= ($i === 0 ? '' : "\n\n") . str_replace(
+                array_map(function($key) { return '%'.$key.'%'; }, array_keys($replace)),
+                array_values($replace),
+                self::TEMPLATE_WORKER
+            );
+        }
+
+        $replace['workers'] = $workers;
         $content = str_replace(
             array_map(function($key) { return '%'.$key.'%'; }, array_keys($replace)),
             array_values($replace),
-            self::TEMPLATE
+            self::TEMPLATE_MAIN
         );
 
         $outputFile = $project_dir . '/worker-supervisor.conf';
